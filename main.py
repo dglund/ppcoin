7d56ed079c953c8148a6dd705986a3df9d4de123
dglund <jesper.dg@gmail.com>
2019-09-22 17:35:15 +0200

    now read nodes list from network.json. Adding via API not possible

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ class Blockchain:
     def __init__(self):
         self.current_transactions = []
         self.chain = []
-        self.nodes = set()
+
+        with open('network.json') as f:
+            data = json.load(f)
+        self.nodes = data['nodes']
 
         # Create the genesis block
         self.new_block(previous_hash='1', proof=100)
